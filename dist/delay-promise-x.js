28333cbe2481f79dc4f4b213e6bbd3e86d83216b
Graham Fairweather <xotic750@gmail.com>
2019-07-27 02:56:52 +0200

    :bookmark: v1.0.23
    Auto build

--- dist/delay-promise-x.js
+++ dist/delay-promise-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2018-present",
-  "date": "2019-07-26T13:13:29.956Z",
+  "date": "2019-07-27T00:56:46.613Z",
   "describe": "",
   "description": "Create a delayed promise.",
   "file": "delay-promise-x.js",
   "hash": "3bdc898d84bec7f33982",
   "license": "MIT",
-  "version": "1.0.22"
+  "version": "1.0.23"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
